29b9c8bc1d8acd7aad92186886f7dcaf71979b79
PericoBoina <pedroaledo1997@gmail.com>
2025-03-23 13:30:06 +0100

    modified:   src/main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,13 @@
-#include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
+#include <stdio.h>
 #include <math.h>
 
 #include "mpu6500.h"
 #include "ws2812.h"
+#include "motorController.h"
 
 #define BUTTON_GPIO GPIO_NUM_23
 
@@ -14,6 +15,7 @@ static const char *TAG = "MAIN";
 
 MPU6500 imu(I2C_NUM_0, GPIO_NUM_21, GPIO_NUM_22);
 WS2812 led(GPIO_NUM_25, 8);
+MotorController motor(GPIO_NUM_16, GPIO_NUM_17, GPIO_NUM_18, GPIO_NUM_19, GPIO_NUM_12, GPIO_NUM_13);
 
 int last_button_state = 0;
 int current_button_state = 0;
@@ -35,7 +37,13 @@ extern "C" void app_main(void)
 
     if (imu.initialize(offset_x, offset_y, offset_z) != ESP_OK)
     {
-        ESP_LOGE(TAG, "Failed to initialize MPU6500");
+        ESP_LOGE(TAG, "ERROR MPU6500");
+        return;
+    }
+
+    if (motor.init() != ESP_OK)
+    {
+        ESP_LOGE(TAG, "ERROR driver de motores");
         return;
     }
 
@@ -56,12 +64,14 @@ extern "C" void app_main(void)
             led.clear();
             led.setPixel(0, 25, 0, 0);
             led.show();
-        }
+            vTaskDelay(pdMS_TO_TICKS(1000));
+                }
 
         imu.updateAngles(angle_x, angle_y, angle_z, offset_x, offset_y, offset_z, last_wake_time);
         ESP_LOGI(TAG, "Angle X: %.2f, Y:  %.2f, Z: %.2f", angle_x, angle_y, angle_z);
         last_button_state = current_button_state;
         led.clear();
+        led.setPixel(0, 0, 25, 0);
         led.show();
         vTaskDelay(pdMS_TO_TICKS(10));
     }
